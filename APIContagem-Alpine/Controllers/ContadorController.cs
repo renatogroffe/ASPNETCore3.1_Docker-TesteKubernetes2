ace390c20190731515798c8716ccd4c2431fed91
Renato Groffe <renato.groffe@yahoo.com.br>
2020-02-26 22:34:57 -0300

    Update ContadorController.cs

--- APIContagem-Alpine/Controllers/ContadorController.cs
+++ APIContagem-Alpine/Controllers/ContadorController.cs
@@ -23,7 +23,7 @@ namespace APIContagem.Controllers
                 {
                     _CONTADOR.ValorAtual,
                     Environment.MachineName,
-                    Local = "Teste",
+                    Local = "Azure Tech Nights",
                     Sistema = Environment.OSVersion.VersionString,
                     Variavel = configuration["TesteAmbiente"],
                     TargetFramework = Assembly
